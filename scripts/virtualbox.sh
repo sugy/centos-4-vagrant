c7e6a6a56d1058efcaf1ba7550fe84cce0550daa
SUGIYAMA Kazutoshi <sugiyama@pepabo.com>
2014-12-25 02:11:48 +0900

    kernel-devel は VBoxGuestAdditions のインストールで必要なため。
    kernel-devel を ks でインストールしていたが、
    VBoxGuestAdditions のインストールで必要になるので、
    このスクリプト内でインストールする。

--- scripts/virtualbox.sh
+++ scripts/virtualbox.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+yum -y update kernel
+yum -y install kernel-devel
+
 mkdir /tmp/vbox
 mount -t iso9660 -o loop /home/vagrant/VBoxGuestAdditions.iso /tmp/vbox
 /tmp/vbox/VBoxLinuxAdditions.run
